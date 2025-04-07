19539e6b4877076f68d2ff5262dd6649f0b8cf51
Humna <humna2004@gmail.com>
2025-04-07 05:54:02 -0400

    Agent 1 identification

--- backend/agents/agent1.py
+++ backend/agents/agent1.py
@@ -1,6 +1,25 @@
-class OtherID():
-    def __init__(self):
-        pass
+from transformers import Blip2Processor, Blip2ForConditionalGeneration
+from PIL import Image
+import torch
 
-    def analyze(self, image_path):
-        return "Analysis result from OtherID"
+# Shoe Identification using BLIP-2 model (FLAN-T5 version)
+class BlipIdentification:
+    def __init__(self):
+        self.processor = Blip2Processor.from_pretrained("Salesforce/blip2-flan-t5-xl")
+        self.model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-flan-t5-xl")
+        self.model.eval()
+
+    # Defining prompt to describe the image 
+    def analyze(self, image_path, prompt="What is in this image?"):
+        
+        # Converting image to RGB for accuracy and compatability with model 
+        image = Image.open(image_path).convert("RGB")
+        inputs = self.processor(images=image, text=prompt, return_tensors="pt")
+
+        # Generating an output (shoe identification results)
+        generated_ids = self.model.generate(**inputs, max_new_tokens=100)
+        generated_text = self.processor.tokenizer.decode(generated_ids[0], skip_special_tokens=True).strip()
+
+        # Returning results 
+        return generated_text
+
