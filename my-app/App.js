3cee01dc32beb68fe0bd3a65281a123075754c21
OmarCodes2 <omarcodes2@gmail.com>
2025-04-08 08:46:52 -0400

    add icons

--- my-app/App.js
+++ my-app/App.js
@@ -9,6 +9,7 @@ import FeedScreen from './screens/FeedScreen';
 import CreatePostScreen from './screens/CreatePostScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import PostDetailScreen from './screens/PostDetailScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -17,11 +18,24 @@ const primaryColor = '#674a99';
 function MainTabs() {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarActiveTintColor: primaryColor,
         tabBarInactiveTintColor: 'gray',
-      }}
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Feed') {
+            iconName = 'home-outline';
+          } else if (route.name === 'Create Post') {
+            iconName = 'add-circle-outline';
+          } else if (route.name === 'Profile') {
+            iconName = 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
     >
       <Tab.Screen name="Feed" component={FeedScreen} />
       <Tab.Screen name="Create Post" component={CreatePostScreen} />
